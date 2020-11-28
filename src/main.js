ef7bd42a98c1232b97ce3bf14185ef0d2baa9f69
Cutepuppets <916660882@qq.com>
2020-11-28 17:04:22 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 //配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -20,6 +24,18 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.component('tree-table',TreeTable)
+//时间过滤器
+Vue.filter('dateFormat', function(originVal){
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth()+1+'').padStart(2,'0')
+  const d = (dt.getDate()+'').padStart(2,'0')
+  const hh = (dt.getHours()+'').padStart(2,'0')
+  const mm = (dt.getMinutes()+'').padStart(2,'0')
+  const ss = (dt.getSeconds()+'').padStart(2,'0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+Vue.use(VueQuillEditor)
 //程序启动的入口JS
 //挂载导入的路由和渲染导入的App跟组件页面
 new Vue({
